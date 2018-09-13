641182c3ea70156119e0115c6dc7ed4e3cb2db08
Diego Martinez <diego.martinez.r@tecsup.edu.pe>
2018-09-13 10:34:29 -0500

    COMMIT SE AGREGO DATOS EN LA CLASE USER.JAVA Y USERCONTROLLER.JAVA

--- Desktop/practica/Practica01/src/main/java/Controller/UserController.java
+++ Desktop/practica/Practica01/src/main/java/Controller/UserController.java
@@ -1,5 +1,20 @@
 package Controller;
-
-public class UserController {
-
+import Model.User;
+public class UserController extends User{
+	User obj = new User();
+	@Override
+	public void setNombre(String nombre) {
+		// TODO Auto-generated method stub
+		super.setNombre(nombre);
+	}
+	@Override
+	public void setApellido(String apellido) {
+		// TODO Auto-generated method stub
+		super.setApellido(apellido);
+	}
+	@Override
+	public void setEdad(int edad) {
+		// TODO Auto-generated method stub
+		super.setEdad(edad);
+	}
 }
--- Desktop/practica/Practica01/src/main/java/Model/User.java
+++ Desktop/practica/Practica01/src/main/java/Model/User.java
@@ -1,5 +1,35 @@
 package Model;
 
 public class User {
-
+	private String nombre;
+	private String apellido;
+	private String paterno;
+	private int edad;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getPaterno() {
+		return paterno;
+	}
+	public void setPaterno(String paterno) {
+		this.paterno = paterno;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+	
 }
